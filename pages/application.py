d51c484bd6ceadf96351c950fdc8e6499154fc03
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-09-06 02:39:23 +0300

    Created app skeleton

--- pages/application.py
+++ pages/application.py
@@ -1,1 +1,19 @@
 """Abstraction of user working with different pages."""
+from pages.login_page import LoginPage
+from pages.user_page import UserPage
+
+
+class Application:
+    """Main application class."""
+
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+        self.login = LoginPage(self)
+        self.user_page = UserPage(self)
+
+    def quit(self):
+        self.driver.quit()
+
+    def open_auth_page(self):
+        self.driver.get(self.url + "/login/index.php")
