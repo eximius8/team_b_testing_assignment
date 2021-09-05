91fe7ba95bd81d12cc69f8e8cda0e4a407295bd9
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-09-06 02:36:20 +0300

    Added auth function to login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,17 @@
 """Страница входа в приложение moodle."""
+from selenium.webdriver.common.by import By
+
 from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
     """Page to check login of users."""
 
-    pass
+    def auth(self, data):
+        """Функция входа в личный кабинет."""
+
+        login = data.username
+        password = data.password
+        self.fill_element(self.find_element((By.ID, "username")), login)
+        self.fill_element(self.find_element((By.ID, "password")), password)
+        self.click_element(self.find_element((By.ID, "loginbtn")))
