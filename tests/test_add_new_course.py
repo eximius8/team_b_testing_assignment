ec9d3aff92bf6979d2cf0f3890cea49df4a0116e
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-09-13 13:01:36 +0300

    Added test skeletons for personal data and new courses.

---
+++ tests/test_add_new_course.py
@@ -0,0 +1,7 @@
+'''Tests for adding new courses live here.'''
+# import pytest
+
+
+class TestAddNewCourse:
+    """Adding new courses."""
+    pass
---
+++ tests/test_add_personal_data.py
@@ -0,0 +1,7 @@
+'''Tests for adding user data live here.'''
+# import pytest
+
+
+class TestAddPersonalData:
+    """Adding new data to person's profile."""
+    pass
