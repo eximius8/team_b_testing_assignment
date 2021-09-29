97fb66084db24db4b35d082b2360261c80d0c1d1
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-09-29 15:44:15 +0300

    Updated precommit for longer linelength

--- conftest.py
+++ conftest.py
@@ -8,13 +8,12 @@ from pages.application import Application
 
 
 @pytest.fixture(scope="session")
-def app():
+def app(request):
     '''Фикстура для открытия браузера.'''
 
-    base_url = 'https://qacoursemoodle.innopolis.university/login/index.php'
-    headless_type = True
-
-    if headless_type:
+    base_url = request.config.getoption("--base-url")
+    headless_type = request.config.getoption("--headless").lower()
+    if headless_type == "true":
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         fixture = Application(
@@ -30,3 +29,25 @@ def app():
     yield fixture
     # Чистим после себя
     fixture.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless",
+        action="store",
+        default="true",
+        help="'true' для режима без видимого браузера,\n"
+        "'false' - для режима где браузер виден",
+    ),
+    parser.addoption(
+        "--base-url",
+        action="store",
+        default="https://qacoursemoodle.innopolis.university",
+        help="url сайта для тестирования",
+    ),
+    parser.addoption(
+        "--username", action="store", default="eximius", help="имя пользователя",
+    ),
+    parser.addoption(
+        "--password", action="store", default="!123456789Ab", help="пароль",
+    ),
