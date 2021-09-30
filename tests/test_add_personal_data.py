f196dbd00e21437ddffea52c612d26fe0bdf03f8
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-09-30 03:30:20 +0300

    Personal data tests

--- tests/test_add_personal_data.py
+++ tests/test_add_personal_data.py
@@ -1,11 +1,14 @@
 '''Tests for adding user data live here.'''
 # import pytest
-from pages.application import Application
+# from models.personal_data import PersonalData
 
 
 class TestAddPersonalData:
     """Adding new data to person's profile."""
 
-    def test_update_profile(self, app: Application):
+    def test_update_profile(self, app, auth):
 
+        app.login.go_to_editing_personal_data()
+        # personal_data = PersonalData.random()
+        # app.user_page._edit_personal_data(personal_data)
         assert True
