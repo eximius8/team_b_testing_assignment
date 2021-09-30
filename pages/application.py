dc15bc62dc46fb47af7434835f90a384ce6b4ec9
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-10-01 01:05:27 +0300

    Added data to application

--- pages/application.py
+++ pages/application.py
@@ -1,4 +1,5 @@
 """Abstraction of user working with different pages."""
+from pages.create_course_page import CreateCoursePage
 from pages.login_page import LoginPage
 from pages.user_page import UserPage
 
@@ -11,9 +12,13 @@ class Application:
         self.url = url
         self.login = LoginPage(self)
         self.user_page = UserPage(self)
+        self.create_course = CreateCoursePage(self)
 
     def quit(self):
         self.driver.quit()
 
     def open_auth_page(self):
         self.driver.get(self.url + "/login/index.php")
+
+    def open_create_course_page(self):
+        self.driver.get(self.url + "/course/edit.php")
