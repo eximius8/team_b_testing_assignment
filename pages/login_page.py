b0c86cba6a3668ff632a7f93f8098e1f0f0b3445
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-09-30 03:14:11 +0300

    Added edit personal info route

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 """Страница входа в приложение moodle."""
 from locators.auth_locators import AuthLocators
+from locators.profile_locators import EditProfileLocators
 from pages.base_page import BasePage
 
 
@@ -19,6 +20,17 @@ class LoginPage(BasePage):
         if len(element) > 0:
             return True
         return False
+
+    def user_menu(self):
+        return self.find_element(AuthLocators.USER_MENU)
+
+    def user_menu_settings(self):
+        return self.find_element(AuthLocators.USER_MENU_SETTINGS)
+
+    def go_to_editing_personal_data(self):
+        self.click_element(self.user_menu())
+        self.click_element(self.user_menu_settings())
+        self.click_element(self.find_element(EditProfileLocators.EDIT_INFO))
 
     def auth(self, data):
         """Функция входа в личный кабинет."""
